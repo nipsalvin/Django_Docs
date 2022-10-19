56d2f2e9d5a3e2b43ad20a5c0b66a13867076f63
nipsalvin <amwaniki.am@gmail.com>
2022-10-19 10:38:50 +0300

    Second Commit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'), 
     #url: .../polls/
-    path('<int:question_id/>', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     #url: .../polls/8/
     path('<int:question_id>/results/', views.results, name='results'),
     #url: .../polls/8/results/
