c0d26fb553c9b6a7e0214a9f248348035c53863f
Sir Mwaniki Alvin IV <68748209+nipsalvin@users.noreply.github.com>
2023-01-26 19:37:42 +0300

    Delete mysite directory

--- mysite/polls/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Choice, Question
-
-admin.site.register(Question) #include model to be added to the admin panel
-admin.site.register(Choice)
-
